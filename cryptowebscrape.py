87a03449453dcc28a57a53194b23e99706344ac8
Corbin Brinkerhoff <cib25@byu.edu>
2025-11-12 19:38:58 -0700

    last commit here i think

--- cryptowebscrape.py
+++ cryptowebscrape.py
@@ -1,10 +1,9 @@
 import requests
 import pandas as pd
 
-# CoinMarketCap public API endpoint
+
 url = "https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing"
 
-# Parameters: start at 1, get top 200 by market cap
 params = {
     "start": 1,
     "limit": 200,
@@ -12,20 +11,16 @@ params = {
     "sortType": "desc"
 }
 
-# Headers (some APIs require User-Agent)
 headers = {"User-Agent": "Mozilla/5.0"}
 
-# Make request
 response = requests.get(url, headers=headers, params=params)
 data = response.json()
 
-# Extract coin list
 coins = data['data']['cryptoCurrencyList']
 
-# Build list of rows
 rows = []
 for coin in coins:
-    quote = coin['quotes'][0]  # USD quote
+    quote = coin['quotes'][0] 
     rows.append({
         "Name": coin['name'],
         "Symbol": coin['symbol'],
@@ -37,11 +32,9 @@ for coin in coins:
         "Circulating Supply": coin['circulatingSupply']
     })
 
-# Build DataFrame
+
 df = pd.DataFrame(rows)
 
-# Save to CSV
 df.to_csv("crypto_data_top_200.csv", index=False)
 
-# Show first 10 rows
 print(df.head(10))
